db0faed22ac32b9187c2192a4405c001261f08e8
abrarazmain <ajmainabrar5@gmail.com>
2023-05-04 12:28:26 +0600

    added chefs

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 const port = 5000
 const cors = require('cors')
-
 app.use(cors())
 
+const allChefs = require('./allDetails.json');
+
 app.get('/', (req, res) => {
-  res.json('Hello World!')
+  res.send('Hellow')
+})
+app.get('/chefs', (req, res) => {
+  res.send(allChefs)
+})
+app.get('/chefs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const single = allChefs.find(s => s.id == id)
+  res.send(single)
 })
 
 app.listen(port, () => {

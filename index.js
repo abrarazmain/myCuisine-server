363fb9d5644de4c949aaaf908ee51ac6c38bf8fb
abrarazmain <ajmainabrar5@gmail.com>
2023-05-04 16:16:53 +0600

    added real img link

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const cors = require('cors')
-app.use(cors())
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+app.use(cors());
 
-const allChefs = require('./allDetails.json');
+const allChefs = require("./allDetails.json");
 
-app.get('/', (req, res) => {
-  res.send('Hellow')
-})
-app.get('/chefs', (req, res) => {
-  res.send(allChefs)
-})
-app.get('/chefs/:id', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hellow");
+});
+app.get("/chefs", (req, res) => {
+  res.send(allChefs);
+});
+app.get("/chefs/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  const single = allChefs.find(s => s.id == id)
-  res.send(single)
-})
+
+  const single = allChefs.find((s) => s.id == id);
+  res.send(single);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
